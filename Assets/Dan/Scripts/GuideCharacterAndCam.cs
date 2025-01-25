daa0d12af450debeb1ddea87cf402e337a0efe10
XenialJesse <44868706+XenialJesse@users.noreply.github.com>
2025-01-25 16:29:52 +0800

    Correct input coords and whitespace cleanup

--- Assets/Dan/Scripts/GuideCharacterAndCam.cs
+++ Assets/Dan/Scripts/GuideCharacterAndCam.cs
@@ -1,30 +1,24 @@
 
-        using UnityEngine;
-        using Unity.FPS.Game;
-        using Unity.FPS.Gameplay;
+using UnityEngine;
+using Unity.FPS.Game;
+using Unity.FPS.Gameplay;
         
-        public class GuideCharacterAndCam : MonoBehaviour
-        {
-            private GameObject m_Character;
-            private Camera m_Camera;
-            [SerializeField] private float m_TravelSpeed = 10f;
-            [SerializeField] private float m_CameraTrailDistance = 3f;
-            [SerializeField] private float m_StrafeSpeed = 4f;
-            [SerializeField] private float m_HorizontalBound = 2f;
-            [SerializeField] private float m_VerticalBound = 1f;
-            
-            PlayerInputHandler m_InputHandler;
-            
-            // Start is called once before the first execution of Update after the MonoBehaviour is created
-            void Start()
-            {
-                m_InputHandler = GetComponent<PlayerInputHandler>();
-
-                if (m_InputHandler == null)
-                {
-                    Debug.Log("Was null");
-                }
-        
+public class GuideCharacterAndCam : MonoBehaviour
+{
+    private GameObject m_Character;
+    private Camera m_Camera;
+    [SerializeField] private float m_TravelSpeed = 10f;
+    [SerializeField] private float m_CameraTrailDistance = 3f;
+    [SerializeField] private float m_StrafeSpeed = 4f;
+    [SerializeField] private float m_HorizontalBound = 2f;
+    [SerializeField] private float m_VerticalBound = 1f;
+    
+    PlayerInputHandler m_InputHandler;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_InputHandler = GetComponent<PlayerInputHandler>();
         DebugUtility.HandleErrorIfNullGetComponent<PlayerInputHandler, PlayerCharacterController>(m_InputHandler,
             this, gameObject);
 
@@ -37,8 +31,8 @@
     void Update()
     {
         Vector3 input = m_InputHandler.GetMoveInput();
-
-        m_Character.transform.position += new Vector3(m_StrafeSpeed * input.x, m_StrafeSpeed * input.y, m_TravelSpeed) * Time.deltaTime;
+        
+        m_Character.transform.position += new Vector3(m_StrafeSpeed * input.x, m_StrafeSpeed * input.z, m_TravelSpeed) * Time.deltaTime;
         
         // Keep character within bounds
         if (m_Character.transform.position.x > m_HorizontalBound)
@@ -58,13 +52,6 @@
             m_Character.transform.position = new Vector3(m_Character.transform.position.x, -m_VerticalBound, m_Character.transform.position.z);
         }
         
-        m_Camera.transform.position = new Vector3(0, 0, m_Character.transform.position.z - m_CameraTrailDistance);
-        
-        Debug.Log(m_InputHandler.GetMoveInput());
-        
-        
-        
-        
-        
+        m_Camera.transform.position = new Vector3(0, 0, m_Character.transform.position.z - m_CameraTrailDistance);  
     }
 }
