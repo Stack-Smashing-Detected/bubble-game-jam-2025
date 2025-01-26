102d764c64db3dbda7b1426f197407f5f99c79fc
kennedgi <kennedgi@oregonstate.edu>
2025-01-26 14:45:43 +0800

    music for menus, some 3d stuff in process of being implemented

--- Assets/Gid/Obstacle.cs
+++ Assets/Gid/Obstacle.cs
@@ -6,7 +6,16 @@ public class Obstacle : MonoBehaviour
     public float health = 1;
     public GameObject effect;
     public bool bubble;
+    public bool forceYCoordinate;
+    public int forcedYCoordinate;
 
+    void Start()
+    {
+        if (forceYCoordinate)
+        {
+            transform.position = new Vector3(transform.position.x, forcedYCoordinate, transform.position.z);
+        }
+    }
 
     public void TakeDamage(float Damage)
     {
